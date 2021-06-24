b935cea7436006c7fb3f748687799cb023c24235
SangeetSarkar <sangeetsarkar2002@gmail.com>
2021-06-24 22:07:29 +0530

    added the index css and js file

---
+++ script.js
@@ -0,0 +1,21 @@
+var index = 0;
+function crack() {
+    if (index == 0) {
+        document.getElementById("eggCrack1").style.display = "block";
+        document.getElementById("eggCrack2").style.display = "block";
+        document.getElementById("eggCrack3").style.display = "block";
+        index++;
+    }
+    else {
+        document.getElementById("eggCrack1").style.display = "none";
+        document.getElementById("eggCrack2").style.display = "none";
+        document.getElementById("eggCrack3").style.display = "none";
+        index--;
+    }
+}
+function crossSection() {
+    // document.getElementById("eggShadow").style.display = "none";
+    document.getElementById("egg").style.left="60vh";
+    document.getElementById("egg").style.animationName="eggAnimation";
+    document.getElementById("yolk").style.display="block";
+}
